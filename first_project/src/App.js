a96a91027f65bedc286f17ab5cc1a00ccffb0b95
Vinicius Correa <viniciuscorrea417@gmail.com>
2023-02-24 15:45:35 -0300

    primeiro exercicio

---
+++ first_project/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <h1>The Walking Dead</h1>
+
+      <hr/>
+
+      <p>
+        The Walking Dead é uma série de televisão dramática e pós-apocalíptica norte-americana, desenvolvida por Frank Darabont, e baseada na série em quadrinhos de mesmo nome de Robert Kirkman, Tony Moore e Charlie Adlard. A série é exibida pelo canal AMC e é protagonizada por Andrew Lincoln como o vice-xerife Rick Grimes da primeira à nona temporada
+      </p>
+
+      <p>
+        A série estreou em 31 de outubro de 2010 exclusivamente no canal a cabo AMC nos Estados Unidos e internacionalmente através do Fox Networks Group e Disney Media and Entertainment Distribution. A décima primeira e última temporada estreou em 22 de agosto de 2021 e terminou em 20 de novembro de 2022.
+      </p>
+    </div>
+  );
+}
+
+export default App;
